2374d485f34bb83fa53313ef3f8008a8392d5c3a
Matt Herold <mattherold@icloud.com>
2024-02-10 17:47:22 +0100

    Changed interrupt handler

--- 01-hearts/InterruptHandler.cc
+++ 01-hearts/InterruptHandler.cc
@@ -7,8 +7,6 @@ extern "C" void arm_gpiote_isr();
 extern "C" uint8_t __stack[];
 extern "C"  void _start(); 
 
-#define i(addr,name)	[(addr)/4] = (void(*)(void)) arm_ ## name ## _isr
-
 extern "C" void arm_hardfault_isr(); 
 extern "C" void arm_pendsv_isr(); 
 extern "C" void arm_debugmon_isr(); 
@@ -25,15 +23,15 @@ __section(".data.init.enter")
 void (* const __my_interrupt_vector[])(void) __attribute((aligned(128))) = {
 	[0] = (void(*)(void))__stack,
 	[1] = (void(*)(void))_start,
-	i(0x08, nmi),
-	i(0x0c, hardfault),
-	i(0x10, memmanage),
-	i(0x14, busfault),
-	i(0x18, usagefault),
-	i(0x2c, svc),
-	i(0x30, debugmon),
-	i(0x38, pendsv),
-	i(0x3c, systick),
+	[2] = arm_nmi_isr, 
+	[3] = arm_hardfault_isr, 
+	[4] = arm_memmanage_isr, 
+	[5] = arm_busfault_isr, 
+	[6] = arm_usagefault_isr, 
+	[11] = arm_svc_isr, 
+	[12] = arm_debugmon_isr, 
+	[14] = arm_pendsv_isr, 
+	[15] = arm_systick_isr,
 	[16 + 6] = arm_gpiote_isr,  // Interrupt 6
 	
 };
